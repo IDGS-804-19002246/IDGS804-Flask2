315c8c84c0db308339ad43e4e788e437db6d6899
Brandon <brandon.djpon000@gmail.com>
2023-02-25 01:16:19 -0600

    2023-02-24

--- forms.py
+++ forms.py
@@ -1,12 +1,32 @@
 from wtforms import Form
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FieldList, FormField, RadioField, SelectField
-
+from wtforms import StringField,PasswordField, SubmitField, FieldList, FormField, RadioField, SelectField
 from wtforms.fields import EmailField
 
+from wtforms import validators
+def mi_validacion(form,field):
+    if len(field.data)==0:
+        raise validators.ValidationError('El campo no tiene datos')
+
 class UserForm(Form):
-    matricula = StringField ('Matricula')
-    nombre = StringField('Nombre')
-    apaterno = StringField('Apaterno')
+    matricula = StringField('Matricula',
+        [validators.DataRequired('El campo es requerido'),
+            validators.length(min=5,max=10,message='Ingresa un min 5 y max 10')])
+    nombre = StringField('Nombre',
+        [validators.DataRequired('El campo es requerido')])
+
+    # matricula = StringField ('Matricula')
+    # nombre = StringField('Nombre')
+    apaterno = StringField('Apaterno',[
+        mi_validacion
+    ])
     amaterno = StringField('Amaterno')
-    emal = EmailField('Correo')
+    email = EmailField('Correo')
+
+class LoginForm(Form):
+    Username = StringField('Usuario',
+        [validators.DataRequired('El campo Usuario es requerido'),
+            validators.length(min=5,max=10,message='Ingresa un min 5 y max 10')])
+    Password = PasswordField('Contraceña',
+        [validators.DataRequired('El campo Contraceña es requerido'),
+            validators.length(min=5,max=10,message='Ingresa un min 5 y max 10')])
